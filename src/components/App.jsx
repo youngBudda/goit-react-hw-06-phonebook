6c35dddf5e8c8aae737b72fba973a98b15a5b376
Yan <young69budda@gmail.com>
2023-09-11 18:48:57 +0300

    qwerty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,26 @@
 import Filter from './Filter/Filter';
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, deleteContact, setFilter } from '../redux/Slice';
 import Form from './Form/Form';
 import ContactList from './ContactList/ContactList';
 
 export function App() {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) || []
-  );
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState(
+  //   () => JSON.parse(localStorage.getItem('contacts')) || []
+  // );
+  const contacts = useSelector(state => state.contacts);
+  // const [filter, setFilter] = useState('');
+  const filter = useSelector(state => state.filter);
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
   const onDeleteContact = id => {
-    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+    // setContacts(prevState => prevState.filter(contact => contact.id !== id));
+    dispatch(deleteContact(id));
   };
 
   const onAddContact = contactData => {
@@ -26,13 +31,14 @@ export function App() {
       alert(`${contactData.name} is already in contacts}`);
       return;
     }
+    dispatch(addContact(contactData));
 
-    const contact = { id: nanoid(), ...contactData };
-    setContacts(prevState => [contact, ...prevState]);
+    // const contact = { id: nanoid(), ...contactData };
+    // setContacts(prevState => [contact, ...prevState]);
   };
 
   const onFilter = filterData => {
-    setFilter(filterData);
+    dispatch(setFilter(filterData));
   };
 
   const filteredContacts = contacts.filter(contact =>
